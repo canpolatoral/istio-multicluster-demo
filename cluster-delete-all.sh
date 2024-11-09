63fc178c5f59e1e95d70f3a2a17262fcfd1a7432
Canpolat Oral <canpoalotral@gmail.com>
2024-11-09 22:05:36 +0400

    removed everything for gh-pages

--- cluster-delete-all.sh
+++
@@ -1,6 +0,0 @@
-set -o xtrace
-set -o errexit
-set -o nounset
-set -o pipefail
-
-kind delete clusters --all
--- run.sh
+++
@@ -1,6 +0,0 @@
-./cluster-delete-all.sh
-./cluster-create.sh
-./vault-configure.sh
-./cert-manager-setup.sh
-./istio-setup.sh
-./argocd-setup.sh
