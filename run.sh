cea010e1499b40f8e6f39fdb88e8e6fe706c4822
Canpolat Oral <canpoalotral@gmail.com>
2024-12-01 23:45:42 +0400

    realm-export added

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 ./cluster-delete-all.sh
 ./cluster-create.sh
-./vault-configure.sh
-./cert-manager-setup.sh
-./istio-setup.sh
+# ./vault-configure.sh
+# ./cert-manager-setup.sh
+# ./istio-setup.sh
 ./argocd-setup.sh
